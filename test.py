5a79544fa3a98c61d7c1b2b895d8dfe38ca4e711
Jonghyun Kim <42399549+BenjaminJonghyun@users.noreply.github.com>
2021-11-29 12:04:03 +0900

    Update test.py

--- test.py
+++ test.py
@@ -3,10 +3,6 @@ Copyright (C) 2019 NVIDIA Corporation.  All rights reserved.
 Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode).
 """
 
-# python test.py --name slic_gsas_test --load_size 256 --crop_size 256 --dataset_mode custom --label_dir datasets/CelebA-HQ/test/labels --image_dir datasets/CelebA-HQ/test/images --label_nc 19 --instance_dir datasets/CelebA-HQ/test/codes_fb --which_epoch 50 --gpu_ids 3
-# python train.py --name slic_gsas_facade_test --gpu_ids 0,1,2,3 --batchSize 32 --load_size 256 --crop_size 256 --dataset_mode custom --label_nc 12 --label_dir datasets/Facade/train/labels --image_dir datasets/Facade/train/images --instance_dir datasets/Facade/train/codes
-#  python test.py --name slic_gsas_city_test --gpu_ids 3 --load_size 256 --crop_size 256 --dataset_mode custom --label_nc 34 --label_dir datasets/city/test/labels --image_dir datasets/city/test/images --instance_dir datasets/city/test/codes --which_epoch 126
-
 import os
 from collections import OrderedDict
 
@@ -35,9 +31,7 @@ webpage = html.HTML(web_dir,
                     (opt.name, opt.phase, opt.which_epoch))
 
 # test
-ssim = 0
-psnr = 0
-nrmse = 0
+
 for i, data_i in enumerate(tqdm(dataloader)):
     if i * opt.batchSize >= opt.how_many:
         break
@@ -51,11 +45,6 @@ for i, data_i in enumerate(tqdm(dataloader)):
                                ('synthesized_image', generated[b]),
                                ('real_image', data_i['image'][b])])
         visualizer.save_images(webpage, visuals, img_path[b:b + 1])
-        #_ssim, _psnr, _nrmse = visualizer.cal_eval(visuals)
-        #ssim += _ssim/500
-        #psnr += _psnr/500
-        #nrmse += _nrmse/500
-#print(ssim, psnr, nrmse)
 webpage.save()
 
 
